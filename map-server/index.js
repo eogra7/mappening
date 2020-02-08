93f9d2087d6a8535f84e9d42463909830e629fd7
Christopher Myers <chessmyers@aol.com>
2020-02-08 02:08:45 -0500

    First commit for backend

---
+++ map-server/index.js
@@ -0,0 +1,38 @@
+import 'dotenv/config'
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors()); //probably don't need if we're using local host (this is useful for accessing foreign domains
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+let users = {
+    1: {
+        id: '1',
+        username: 'Robin Wieruch',
+    },
+    2: {
+        id: '2',
+        username: 'Dave Davids',
+    },
+};
+
+app.get('/', (req, res) => {
+    return res.send(Object.values(users));
+});
+app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+});
+app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+});
+app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+});
+
+app.listen(process.env.BACKEND_PORT, () =>
+    console.log('Example app listening on port ' + process.env.BACKEND_PORT),
+);
