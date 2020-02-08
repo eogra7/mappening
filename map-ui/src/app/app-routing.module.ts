4c76d28532ee01b093d5f37c1d2e314f652fac84
Evan Ogra <eogra99@gmail.com>
2020-02-08 00:07:06 -0500

    feat(ng): add home component

--- map-ui/src/app/app-routing.module.ts
+++ map-ui/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
 
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'map'},
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: 'home', component: HomeComponent},
   {path: 'map', loadChildren: () => import('./map/map.module').then(m => m.MapModule)}
 ];
 
--- map-ui/src/app/map/map.component.ts
+++ map-ui/src/app/map/map.component.ts
@@ -2,7 +2,19 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-map',
-  templateUrl: './map.component.html'
+  templateUrl: './map.component.html',
+  styles: [
+    `:host {
+          width: 100vw;
+          height: 100vh;
+          background-color: lightblue;
+      }
+
+      p.placeholder-text {
+          margin-left: auto;
+          margin-right: auto;
+      }`
+  ]
 })
 export class MapComponent {
 
